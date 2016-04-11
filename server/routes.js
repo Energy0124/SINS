41759a14a489530d5f0b36f56dacf801b331c6b8
Yorkyat <s1155062584@cuhk.edu.hk>
2016-04-11 18:36:17 +0800

    add notification api endpoint

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
+  app.use('/api/notifications', require('./api/notification'));
   app.use('/api/lobbies', require('./api/lobby'));
   app.use('/api/messages', require('./api/message'));
   app.use('/api/articles', require('./api/article'));
